55d5a9a6b13c9982569cf164533b4c4ca35f930c
Esther Sanchez <sanchez.ther@gmail.com>
2017-03-26 12:10:02 +0200

    arguments check & usage

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	fileName, err := parseArgs()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		usage()
 		os.Exit(1)
 	}
 	fmt.Println(fileName)
@@ -16,5 +17,12 @@ func main() {
 }
 
 func parseArgs() (string, error) {
+	if len(os.Args) == 1 {
+		return "", fmt.Errorf("Invalid number of arguments (%d)", len(os.Args)-1)
+	}
 	return "", fmt.Errorf("TODO")
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage:\n\tcat file")
 }
